28acbfecf1a01b10605b019ba0e24b2c54bdd4f5
zxin <2932554779@qq.com>
2023-05-15 23:22:06 +0800

    添加: 学生模块通过姓名进行搜索功能

--- src/main/java/com/cuit9622/olms/service/StudentService.java
+++ src/main/java/com/cuit9622/olms/service/StudentService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cuit9622.common.model.R;
 import com.cuit9622.olms.entity.Student;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.cuit9622.olms.model.StudentSelectModel;
 import com.cuit9622.olms.vo.NoticeVo;
 import com.cuit9622.olms.vo.StudentVo;
 
@@ -22,7 +23,7 @@ public interface StudentService extends IService<Student> {
      * @return
      * @Date
      */
-     R<Page<StudentVo>> selectStudents(Integer pageSize, Integer page);
+     Page<StudentVo> selectStudents(Integer pageSize, Integer page, StudentSelectModel model);
 
     /**
      * @Description 得到学生的信息
