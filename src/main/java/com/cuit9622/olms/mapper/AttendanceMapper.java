20659f92d4ff186a5172da9305ebe05bbb80bc80
Wed0n <wed0n1213@gmail.com>
2023-05-22 16:23:58 +0800

    新功能: 增加学生查询考勤记录

---
+++ src/main/java/com/cuit9622/olms/mapper/AttendanceMapper.java
@@ -0,0 +1,19 @@
+package com.cuit9622.olms.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cuit9622.olms.entity.Attendance;
+import com.cuit9622.olms.vo.AttendanceVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author Wed0n
+ * @description 针对表【sys_attendance(签到表)】的数据库操作Mapper
+ * @createDate 2023-05-20 19:58:21
+ * @Entity com.cuit9622.olms.entity.Attendance
+ */
+@Mapper
+public interface AttendanceMapper extends BaseMapper<Attendance> {
+    Page<AttendanceVo> getTargetAttendance(@Param("page") Page<AttendanceVo> page, @Param("userId") Integer userId);
+}
