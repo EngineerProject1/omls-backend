1fccdbffb0adcfaf7595f00683d95146238fb5c6
lsh <3040114965@qq.com>
2023-06-07 15:28:45 +0800

    注释：添加swagger注释

--- src/main/java/com/cuit9622/olms/vo/AttendanceForAppointmentVo.java
+++ src/main/java/com/cuit9622/olms/vo/AttendanceForAppointmentVo.java
@@ -1,15 +1,22 @@
 package com.cuit9622.olms.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@ApiModel("考勤数据传输对象")
 public class AttendanceForAppointmentVo {
+    @ApiModelProperty("状态")
     private String status;
+    @ApiModelProperty("姓名")
     private String realName;
+    @ApiModelProperty("学号/工号")
     private String username;
+    @ApiModelProperty("更新时间")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 }
--- src/main/java/com/cuit9622/olms/vo/AttendanceForPersonVo.java
+++ src/main/java/com/cuit9622/olms/vo/AttendanceForPersonVo.java
@@ -2,6 +2,8 @@ package com.cuit9622.olms.vo;
 
 import com.cuit9622.olms.entity.Attendance;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,11 +12,17 @@ import java.sql.Time;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@ApiModel("个人考勤数据传输对象")
 public class AttendanceForPersonVo extends Attendance {
+    @ApiModelProperty("实验名称")
     private String experimentName;
+    @ApiModelProperty("实验室名字")
     private String labName;
+    @ApiModelProperty("预约时间")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date bookTime;
+    @ApiModelProperty("预约时间段的开始时间")
     private Time startTime;
+    @ApiModelProperty("预约时间段的结束时间")
     private Time endTime;
 }
